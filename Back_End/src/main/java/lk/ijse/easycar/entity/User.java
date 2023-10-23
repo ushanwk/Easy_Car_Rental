819b185d832caa6ce0b923e8cda1aff1d4a97e26
UshanKaushalya <Ushanwk22@gmail.com>
2023-10-23 20:43:28 +0530

    Back End Table Creation

---
+++ Back_End/src/main/java/lk/ijse/easycar/entity/User.java
@@ -0,0 +1,23 @@
+package lk.ijse.easycar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class User {
+
+    @Id
+    private String username;
+    private String Password;
+    private String Role;
+
+}
