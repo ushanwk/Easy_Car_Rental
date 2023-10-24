dbdddf233d040f516b3db9bd7f5932e342732f6d
UshanKaushalya <Ushanwk22@gmail.com>
2023-10-24 18:18:24 +0530

    Entities creating

---
+++ Back_End/src/main/java/lk/ijse/easycar/entity/Customer.java
@@ -0,0 +1,32 @@
+package lk.ijse.easycar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.context.annotation.Primary;
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+
+public class Customer {
+
+    @Id
+    private String customerID;
+    private String name;
+    private String address;
+    private String email;
+    private String contactNo;
+    private String licenseNo;
+    private String frontImage;
+    private String backImage;
+
+    @OneToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "userName",referencedColumnName = "userName",nullable = false)
+    private User userName;
+
+}
