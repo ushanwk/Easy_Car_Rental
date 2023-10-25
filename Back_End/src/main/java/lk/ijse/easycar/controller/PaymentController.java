31aed3e0a26587de6adacbeb5c81fb3a663c234e
UshanKaushalya <Ushanwk22@gmail.com>
2023-10-25 13:06:12 +0530

    Back end COntrollers initializing complete

---
+++ Back_End/src/main/java/lk/ijse/easycar/controller/PaymentController.java
@@ -0,0 +1,14 @@
+package lk.ijse.easycar.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin
+public class PaymentController {
+
+
+
+}
