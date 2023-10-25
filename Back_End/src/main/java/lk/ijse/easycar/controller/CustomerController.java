5e1e5fd467d8aa40bce366a837a4c1779a8dd0d4
UshanKaushalya <Ushanwk22@gmail.com>
2023-10-25 13:43:17 +0530

    Back end add customer function starting

--- Back_End/src/main/java/lk/ijse/easycar/controller/CustomerController.java
+++ Back_End/src/main/java/lk/ijse/easycar/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package lk.ijse.easycar.controller;
 
+import lk.ijse.easycar.dto.CustomerDTO;
 import lk.ijse.easycar.service.CustomerService;
+import lk.ijse.easycar.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,11 @@ public class CustomerController {
 
     @Autowired
     CustomerService customerService;
+
+    @PostMapping
+    public ResponseUtil addCustomer(CustomerDTO customerDTO){
+        customerService.addCustomer(customerDTO);
+        return new ResponseUtil("Ok", "Customer Successfully Added", customerDTO);
+    }
 
 }
--- Back_End/src/main/java/lk/ijse/easycar/service/impl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/easycar/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,11 @@
 package lk.ijse.easycar.service.impl;
 
 import lk.ijse.easycar.dto.CustomerDTO;
+import lk.ijse.easycar.entity.Customer;
+import lk.ijse.easycar.repo.CustomerRepo;
 import lk.ijse.easycar.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,9 +14,15 @@ import javax.transaction.Transactional;
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    CustomerRepo customerRepo;
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public void addCustomer(CustomerDTO customerDTO) {
-
+        Customer customer = mapper.map(customerDTO, Customer.class);
+        customerRepo.save(customer);
     }
 
 }
