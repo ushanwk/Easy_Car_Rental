e91bb5e1185587dcf7222b961c779439c7e5ab94
UshanKaushalya <Ushanwk22@gmail.com>
2023-10-28 16:36:26 +0530

    Customer rent page front end

---
+++ Front_End/assest/js/CustomerRentPageJS.js
@@ -0,0 +1,14 @@
+for (var i = 0; i<5; i++){
+
+    let car =
+        ` 
+ <div class="col-3" style="height: 40vh; border: 1px black solid">
+    
+    
+    
+ </div>
+    `;
+
+    $('#cars').append(car);
+
+}
