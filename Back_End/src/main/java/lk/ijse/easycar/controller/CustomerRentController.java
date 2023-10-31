7046ba9c0a3e7eca1424b1d5c7c06ba0dd3cab64
UshanKaushalya <Ushanwk22@gmail.com>
2023-10-31 21:42:40 +0530

    Place rent developing

--- Back_End/src/main/java/lk/ijse/easycar/controller/CustomerRentController.java
+++ Back_End/src/main/java/lk/ijse/easycar/controller/CustomerRentController.java
@@ -1,10 +1,13 @@
 package lk.ijse.easycar.controller;
 
 import lk.ijse.easycar.dto.CustomerDTO;
+import lk.ijse.easycar.dto.RentDetailDTO;
 import lk.ijse.easycar.dto.RentalDTO;
 import lk.ijse.easycar.dto.UserDTO;
 import lk.ijse.easycar.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customerrent")
@@ -14,6 +17,11 @@ public class CustomerRentController {
     @PostMapping
     public ResponseUtil addRent(@RequestBody RentalDTO rentalDTO){
 
+        List<RentDetailDTO> rentDetails = rentalDTO.getRentDetails();
+
+        for (int i = 0; i < rentDetails.size(); i++){
+            System.out.println(rentDetails.get(i).getCarID());
+        }
 
         return new ResponseUtil("Ok", "Customer Successfully Added", null);
 
